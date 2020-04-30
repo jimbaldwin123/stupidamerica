17aaf0f69130d288e05a66dc8f4084eaaeb3fe11
Jim Baldwin <jim@netvendor.net>
2020-04-30 14:00:29 -0700

    set up cron job

--- wp-content/plugins/my-wp-cron-test/my-wp-cron-test.php
+++ wp-content/plugins/my-wp-cron-test/my-wp-cron-test.php
@@ -7,8 +7,8 @@ function bl_print_tasks() {
     echo '<pre>'; print_r( _get_cron_array() ); echo '</pre>';
 }
 
-add_action( 'my_hourly_event',  'sa_notify_save_post_cron' );
-wp_schedule_event( time(), 'hourly', 'my_hourly_event' );
+// add_action( 'my_hourly_event',  'sa_notify_save_post_cron' );
+// wp_schedule_event( time(), 'hourly', 'my_hourly_event' );
 
 /*
 public static function activate() {
