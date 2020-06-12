6466b33f9fa9436ee52eab7bde8e6ee33a1ade2f
Jim Baldwin <jim@netvendor.net>
2020-06-12 12:21:51 -0700

    fix missing remove_smart_quotes function

--- wp-content/themes/newsly-magazine/functions-sa.php
+++ wp-content/themes/newsly-magazine/functions-sa.php
@@ -17,7 +17,20 @@ define('DB_COLLATE', '');
 /**
 notify of stupidest page update
 **/
-include('./wp-content/themes/newsly-magazine/functions.php');
+
+function remove_smart_quotes($content) {
+
+    $content= str_replace(
+        array("\xe2\x80\x98", "\xe2\x80\x99", "\xe2\x80\x9c", "\xe2\x80\x9d", "\xe2\x80\x93", "\xe2\x80\x94", "\xe2\x80\xa6"),
+        array("'", "'", '"', '"', '-', '--', '...'), $content);
+
+    $content= str_replace(
+        array(chr(145), chr(146), chr(147), chr(148), chr(150), chr(151), chr(133)),
+        array("'", "'", '"', '"', '-', '--', '...'), $content);
+
+    return $content;
+}
+
 function sa_notify_save_post($post,$slug) {
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
     $content = substr(strip_tags($post['post_content']),0,250) . '...';
